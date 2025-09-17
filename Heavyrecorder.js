c7201c4f90dea6b0581df71fd2c3323fc328d96d
Mythreon <magsymaggie@gmail.com>
2025-09-17 16:12:48 +0300

    Update Heavyrecorder.js

--- Heavyrecorder.js
+++ Heavyrecorder.js
@@ -2,3 +2,5 @@
 // In script.js (the standard one), you have to manually transport the video to MOV or MP4.
 // In this one, it is done automatically, but it is CPU usage heavy.
 
+// This one also has more features, and is better than the regular recorder.
+
