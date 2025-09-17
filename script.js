b35b2371c8fc465e160ca3cf07f24dce0fd6b956
Mythreon <magsymaggie@gmail.com>
2025-09-17 16:05:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Mythreon 2025
+
+navigator.mediaDevices.getDisplayMedia({ video: true })
+  .then(function(stream) {
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = function(event) {
+      let videoBlob = event.data;
+      let videoURL = URL.createObjectURL(videoBlob);
+      let a = document.createElement('a');
+      a.href = videoURL;
+      a.download = 'recording.webm';
+      a.click();
+    };
+    mediaRecorder.start();
+    setTimeout(() => mediaRecorder.stop(), 600000);  // stops after 10 minutes automatically. You can increase/decrease this to your liking.
+  })
+  .catch(function(error) {
+    console.error('Error capturing screen:', error);
+  });
+
