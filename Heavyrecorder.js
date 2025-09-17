65491ce9c30fff771fe1e00b6b4b95182a05a6f1
Mythreon <magsymaggie@gmail.com>
2025-09-17 16:11:36 +0300

    Create Heavyrecorder.js

---
+++ Heavyrecorder.js
@@ -0,0 +1,4 @@
+// What's the difference between the two recorders?
+// In script.js (the standard one), you have to manually transport the video to MOV or MP4.
+// In this one, it is done automatically, but it is CPU usage heavy.
+
